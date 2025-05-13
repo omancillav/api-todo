d3cc3f6ca7fff63692c7a477991f647579d9d508
MancillaO <edgarmncllav04@gmail.com>
2025-05-13 00:42:43 -0600

    Refactor usersRouter to improve middleware organization and remove duplicate user creation route

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,12 +1,14 @@
 import { Router } from 'express'
 import { UserController } from '../controllers/usersController.js'
+import { authenticateToken } from '../middlewares/auth.js'
 
 export const usersRouter = Router()
+usersRouter.post('/', UserController.createUser)
+
+usersRouter.use(authenticateToken)
 
 usersRouter.get('/', UserController.getAll)
 usersRouter.get('/:id', UserController.getById)
-
-usersRouter.post('/', UserController.createUser)
 
 usersRouter.patch('/:id', UserController.updateUser)
 usersRouter.delete('/:id', UserController.deleteUser)

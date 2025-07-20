e2cfc91ffe3cde8eaa358480f340ed72dac29844
MancillaO <edgarmncllav04@gmail.com>
2025-07-19 23:32:58 -0600

    refactor accepted origins

--- config.js
+++ config.js
@@ -7,5 +7,7 @@ export const {
   MYSQL_TOKEN,
   NODE_ENV,
   logger = NODE_ENV === 'production' ? 'combined' : 'dev',
-  JWT_SECRET
+  JWT_SECRET,
+  ALLOWED_ORIGINS: envOrigins = 'http://127.0.0.1:5173',
+  allowedOrigins = envOrigins.split(',').map(origin => origin.trim())
 } = process.env
--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,19 +1,18 @@
 import cors from 'cors'
+import { allowedOrigins } from '../../config.js'
 
 export const corsMiddleware = () => cors({
   origin: (origin, callback) => {
-    const ACCEPTED_ORIGINS = [
-      'http://127.0.0.1:5500',
-      'https://app-todo-gamma.vercel.app',
-      'https://app-todo-git-develop-mancillaos-projects.vercel.app'
-    ]
-
-    if (ACCEPTED_ORIGINS.includes(origin)) {
-      return callback(null, true)
-    }
     if (!origin) {
       return callback(null, true)
     }
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true)
+    }
+
     return callback(new Error('Not allowed by CORS'))
-  }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200
 })
